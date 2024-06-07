defc89f5e831dab1dd1558504ff7d0bda3da9d82
glodypingedi <glodykabengele55@gmail.com>
2024-06-07 15:31:07 +0100

    gestion de la modification d'un événement avec le formulaire dans blade (Edit)

--- resources/views/evenements/index.blade.php
+++ resources/views/evenements/index.blade.php
@@ -2,10 +2,10 @@
     <x-slot name="header">
         <div class="flex justify-between items-center" >
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Liste des compétitions') }}
+                {{ __('Liste des événements') }}
             </h2>
             <a href="{{route('evenements.create')}}" class="text-yellow-400 hover:text-white border border-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-yellow-300 dark:text-yellow-300 dark:hover:text-white dark:hover:bg-yellow-400 dark:focus:ring-yellow-900">
-            Créer une compétition
+            Créer un événement
             </a>
         </div>
     </x-slot>
@@ -62,10 +62,10 @@
                         <div id="dropdownDots{{$i}}" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
                             <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownMenuIconButton">
                             <li>
-                                <a href="" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Editer</a>
+                                <a href="{{route('evenements.edit', $item->id)}}" class="block px-4 py-2 hover:bg-blue-100 dark:hover:bg-blue-600 dark:hover:text-white">Editer</a>
                             </li>
                             <li>
-                                <a" data-modal-target="delete-modal" data-modal-toggle="delete-modal" href="" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Supprimer</a>
+                                <a data-modal-target="delete-modal" data-modal-toggle="delete-modal" href="" class="block px-4 py-2 hover:bg-red-100 dark:hover:bg-red-600 dark:hover:text-white">Supprimer</a>
                             </li>
                         </div>
                     </td>
