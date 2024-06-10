bce953184b17437d197dcbec650e1146b614fdb2
yoshi <glodykabengele55@gmail.com>
2024-06-10 01:49:40 +0100

    création de l'Api evenement ainsi que Swagger

---
+++ app/Http/Controllers/Api/EvenementController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Evenement;
+use Illuminate\Http\Request;
+
+class EvenementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    /**
+     * @OA\Get(
+     *      path="/evenements",
+     *      operationId="getEvenementsList",
+     *      tags={"Evenements"},
+     *      summary="Afficher la liste des evenements",
+     *      description="Api qui nous retourne la liste des evenements",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Not Found"
+     *      )
+     *     )
+     */
+    public function index()
+    {
+        $evenements = Evenement::all();
+        return response()->json($evenements);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Evenement $evenement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Evenement $evenement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Evenement $evenement)
+    {
+        //
+    }
+}
