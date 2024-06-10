7126727363324197a9d862ba1e22c45d3bc4ed68
MrAlainZoura <t.alain1312@gmail.com>
2024-06-10 09:46:00 +0100

    Creation migration, factory et seeders de phase

---
+++ database/factories/PhaseFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Phase>
+ */
+class PhaseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            "nom"=> $this->faker->words(3, true),
+            "description"=> $this->faker->words(3, true),
+            "statut"=> $this->faker->randomElement(["active","desactive"]),
+            "date_debut"=> $this->faker->dateTimeBetween($startDate = 'now', $endDate = '1 year', $timezone = null),
+            "date_fin"=> $this->faker->dateTimeBetween($startDate = 'now', $endDate = '1 year', $timezone = null),
+            "evenement_id"=> $this->faker->numberBetween(1,5),
+        ];
+    }
+}
