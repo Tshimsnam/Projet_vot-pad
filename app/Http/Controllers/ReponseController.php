ae60b78e4f4948b5d8e799206d9e074848ac918d
MrAlainZoura <t.alain1312@gmail.com>
2024-06-13 13:57:31 +0100

    modification migration phase et controller reponse

--- app/Http/Controllers/ReponseController.php
+++ app/Http/Controllers/ReponseController.php
@@ -18,7 +18,7 @@ class ReponseController extends Controller
         // $question= Question::orderBy("id","desc")->paginate(1);
         // $assertion=Assertion::orderBy("id","desc")->where("question_id",)->get();
 
-        $questionAssertion1 = DB::table('assertions')
+        $questionAssertion = DB::table('assertions')
             ->join('questions', 'assertions.question_id', '=', 'questions.id')
             ->select(
                 'questions.id as question_id',
@@ -29,17 +29,7 @@ class ReponseController extends Controller
             ->where('questions.statut','=','valide')
             ->where('assertions.statut','=','active')
             ->get();
-        $questionAssertion3 =[];
-       foreach ($questionAssertion1 as  $key=> $questionAssertion) {
-
-            $questionAssertion2 = Assertion::find($questionAssertion1->question_id);
-
-            foreach ($questionAssertion2 as $key2 => $questionAssertion3) {
-                array_push($questionAssertion3,  implode(",",$questionAssertion2));
-            }
-            
-       }
-       $questionAssertion=$questionAssertion3;
+    
         return view("reponses.index", compact('questionAssertion'));
     }
 
