62f3037438a89533bb92063d69e825aa99003098
MrAlainZoura <t.alain1312@gmail.com>
2024-07-10 13:42:39 +0100

    finalisation de reponse : une question à la fois et enregistrement

--- app/Http/Controllers/ReponseController.php
+++ app/Http/Controllers/ReponseController.php
@@ -54,12 +54,14 @@ class ReponseController extends Controller
     public function store(StoreReponseRequest $request)
     {
         $reponse=$request->id_collection_keyQuestion_valAssertion;
+       
         $intervenant=$request->intervenant_id;
         $phase_id=$request->phase_id;
         if(!empty($reponse)){
             foreach ($reponse as $key => $value) { 
                 $question_id=$key;
                 $assertion_id=$value;
+                
                 // dd($question_id, $assertion_id, $intervenant,$phase_id);
                 $ponderationQuestion=DB::table('question_phases')
                                     ->select('ponderation')
@@ -70,19 +72,22 @@ class ReponseController extends Controller
                                     ->select('id','ponderation')
                                     ->where('question_id',$question_id)
                                     ->get();
+                
                 $tabPonderation=array();
                 foreach ($allAssertionQuestion as $key => $value) {
                     array_push($tabPonderation,$value->ponderation);
                 }
+                
                 $maxAssertion=max($tabPonderation); 
+                
                 $ponderationAssertionChoisie=DB::table('assertions')
                                             ->select('ponderation')
                                             ->where('id',$assertion_id)
                                             ->get();
                 //traitement pour trouver cote
-                
                 $Pdq=$ponderationQuestion[0]->ponderation;
                 $Pda=$ponderationAssertionChoisie[0]->ponderation;
+                // dd($maxAssertion, $ponderationAssertionChoisie, $Pdq);
                 $cote=round($Pdq*$Pda/$maxAssertion,2);//on prend 2 rangs apres la virgule
                 //sauvegarde dans la base de donnees
                 $saveReponse=Reponse::firstOrCreate([
--- resources/views/reponses/index.blade.php
+++ resources/views/reponses/index.blade.php
@@ -8,7 +8,6 @@
         </div>
         
     </x-slot>
-    
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
@@ -47,17 +46,12 @@
                                 </form>
                             </div>
                         @else
-                        <div class="justify-self-center items-center">
+                        <div class="justify-self-center items-center text-center">
                             <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{session('phase')->nom}}</h5>
-                          
-                                <input type="text" name="phase_id" id="" class="hidden" value="3">
-                                
-                                
-                                
-                                <label for="envoi_reponse" class="px-3 py-2 w-full text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Terminer</label>
-                            
+                            <input type="hidden" name="tableau_qst_asrt" id="tab_qst_qsr" value="{{json_encode(session('questionAssetionTab'))}}">
+                            <label for="envoi_reponse" class="grid grid-cols-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Terminer</label>
                         </div>
-                       
+       
                         <div >
                             <div id="dropdownTimepicker" class="z-10 my-5  bg-white rounded-lg shadow w-auto dark:bg-gray-700 p-3">
                                 <div class="max-w-[16rem] mx-auto grid grid-cols-2 gap-4 mb-2">
@@ -90,36 +84,158 @@
                             </div>
                         </div>
                     </div>
-                    <form action="{{route('reponses.store')}}" method="post">
+                   
+                    <form action="{{route('reponses.store')}}" method="post" class="relative">
                         @csrf
                         @method('post')
-                        <input type="text" name="intervenant_id" id="" class="hidden" value="1">
+                        <input type="text" name="intervenant_id" id="" class="hidden" value="10">
                         <input type="text" name="phase_id" id="" class="hidden" value="3">
+                        
                         @foreach (session('questionAssetionTab') as $key=> $value )
-                        <div class="p-6 my-5 mx-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                            <label>
-                            <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-168h66.67v168h168V-120h-168Zm586.66 0h-168v-66.67h168v-168H840v168q0 27-19.83 46.84Q800.33-120 773.33-120ZM120-773.33q0-27 19.83-46.84Q159.67-840 186.67-840h168v66.67h-168v168H120v-168Zm720 0v168h-66.67v-168h-168V-840h168q27 0 46.84 19.83Q840-800.33 840-773.33Zm-361.98 530q17.65 0 29.81-12.19Q520-267.7 520-285.35t-12.19-29.82q-12.18-12.16-29.83-12.16t-29.81 12.18Q436-302.96 436-285.31q0 17.64 12.19 29.81 12.18 12.17 29.83 12.17ZM444-393h62.33q0-30.67 9-50t35.34-45.67q33-33 45.83-57.16 12.83-24.17 12.83-53.22 0-52.28-36.74-84.95t-93.26-32.67q-50 0-86.66 24.34Q356-668 339.33-622l57.34 23.67q11.66-26 33.5-41.84Q452-656 479.35-656q30.5 0 48.91 16.5 18.41 16.5 18.41 42.5 0 20-10.5 38.17-10.5 18.16-35.5 41.16-33 31-44.84 56.34Q444-436 444-393Z"/></svg>
-                                <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{$value['question']['question']}}?</h5>
-                            </label>
-                            @foreach ($value['assertion'] as $key1=>$assertions )
-                            @foreach ( $assertions as $i=>$var)
-                            <div class="flex items-center ps-4 my-1 border border-gray-200 rounded dark:border-gray-700">
-                                <input id="{{$var->id}}" type="radio" value="{{$var->id}}" name="id_collection_keyQuestion_valAssertion[{{$value['question']['id']}}]" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                                <label for="{{$var->id}}" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{$var->assertion}}</label>
-                            </div>
-                            @endforeach
-                            @endforeach
-                        </div>   
-                            
+                            <div id="{{$value['question']['question']}}" value="{{$value['question']['question']}}" class="hidden">
+                                <label>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#e8eaed"><path d="M186.67-120q-27 0-46.84-19.83Q120-159.67 120-186.67v-168h66.67v168h168V-120h-168Zm586.66 0h-168v-66.67h168v-168H840v168q0 27-19.83 46.84Q800.33-120 773.33-120ZM120-773.33q0-27 19.83-46.84Q159.67-840 186.67-840h168v66.67h-168v168H120v-168Zm720 0v168h-66.67v-168h-168V-840h168q27 0 46.84 19.83Q840-800.33 840-773.33Zm-361.98 530q17.65 0 29.81-12.19Q520-267.7 520-285.35t-12.19-29.82q-12.18-12.16-29.83-12.16t-29.81 12.18Q436-302.96 436-285.31q0 17.64 12.19 29.81 12.18 12.17 29.83 12.17ZM444-393h62.33q0-30.67 9-50t35.34-45.67q33-33 45.83-57.16 12.83-24.17 12.83-53.22 0-52.28-36.74-84.95t-93.26-32.67q-50 0-86.66 24.34Q356-668 339.33-622l57.34 23.67q11.66-26 33.5-41.84Q452-656 479.35-656q30.5 0 48.91 16.5 18.41 16.5 18.41 42.5 0 20-10.5 38.17-10.5 18.16-35.5 41.16-33 31-44.84 56.34Q444-436 444-393Z"/></svg>
+                                    <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">{{$value['question']['question']}}?</h5>
+                                </label>
+                                @foreach ($value['assertion'] as $key1=>$assertions )
+                                    @foreach ( $assertions as $i=>$var)
+                                        <div class="flex items-center ps-4 my-1 border border-gray-200 rounded dark:border-gray-700">
+                                            <input id="{{$var->id}}" type="radio" value="{{$var->id}}" name="id_collection_keyQuestion_valAssertion[{{$value['question']['id']}}]" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                            <label for="{{$var->id}}" class="w-full py-4 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{$var->assertion}}</label>
+                                        </div>
+                                    @endforeach
+                                @endforeach
+                            </div>   
                         @endforeach
-                        <button type="submit" id="envoi_reponse">Terminer</button>
-                    </form>
+                        <div class="z-10 my-5 flex justify-between bg-white rounded-lg shadow w-auto dark:bg-gray-700 p-3">
+                            <!-- Slider controls -->
+                            <button type="button" id="precedent" class=" top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
+                                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                                    </svg>
+                                    <span class="sr-only">Previous</span>
+                                </span>
+                            </button>
+                            <button type="button"  id="suivant" class="top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" >
+                                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                                    </svg>
+                                    <span class="sr-only">Next</span>
+                                </span>
+                            </button>
+                            <button type="submit" id="envoi_reponse" class="hidden">Terminer</button>
+                        </div>
+                    </form>                   
                 </div>
                 @endif
             </div>
-        </div>
+        </div>      
     </div>
+    
     <script>
+        //script pour afficher une question à la fois
+        const data1=document.getElementById('tab_qst_qsr').value;
+        if (data1) {
+  
+            const data =JSON.parse(data1);
+            const tableauJSON =JSON.parse(data1);
+            console.log(tableauJSON);
+        
+            function afficherElement(index, hidden, precedent_item) {
+                
+                Fields=document.getElementById("InputFields");
+                
+                const item = tableauJSON[index];
+                console.log("Question :");
+                console.log(item.question.question)
+
+                if(precedent_item==null){
+                var question_actu = item.question.question;
+                var question_visible = document.getElementById(item.question.question);
+                    var question_vue_form = question_visible.getAttribute('value');
+                        question_visible.className=hidden;
+                }else{
+                    const item2 = tableauJSON[precedent_item]
+                    var question_actu_pre = item2.question.question;
+                    var question_visible_pre = document.getElementById(item2.question.question);
+                    var question_vue_form_prev = question_visible_pre.getAttribute('value');
+                        question_visible_pre.className = "hidden";
+
+                    var question_actu = item.question.question;
+                    var question_visible = document.getElementById(item.question.question);
+                    var question_vue_form = question_visible.getAttribute('value');
+                        question_visible.className=hidden;
+                }  
+           
+                console.log("Assertions :");
+
+                    item.assertion.forEach(assertionGroup => {
+                    assertionGroup.forEach(assertion => {
+                        console.log(`- Assertion ${assertion.id}: ${assertion.assertion}`);
+                    });
+                    
+                });
+                console.log("---");
+            }
+            let currentIndex = 0; // Indice de l'élément actu au chargement
+            let hideClass = "p-6 my-5 mx-5 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            //classe au chargement
+            let element_precedent = null; //au chargement
+            const buttonPrecedent = document.getElementById('precedent');
+            const buttonSuivant = document.getElementById('suivant');
+            const buttonTerminer =document.getElementById('envoi_reponse');
+            if(currentIndex <= 0){
+                buttonPrecedent.className='hidden'
+                buttonTerminer.className = "hidden"
+            }
+              
+            console.log(afficherElement(currentIndex, hideClass, element_precedent));// Afficher le premier élément au démarrage
+            
+            // Bouton Suivant
+            document.getElementById("suivant").addEventListener("click", () => {
+            
+            if(currentIndex==tableauJSON.length-1)
+            {
+                buttonSuivant.className = "hidden";
+                buttonTerminer.className = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                console.log('fin')
+
+            }else {
+                currentIndex = (currentIndex + 1);
+                buttonTerminer.className = "hidden"
+                buttonPrecedent.className = "top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                element_precedent = currentIndex - 1;
+                afficherElement(currentIndex, hideClass, element_precedent)
+            }
+            });
+
+            // Bouton Précédent
+            document.getElementById("precedent").addEventListener("click", () => {
+            if(currentIndex==0)
+            {
+                buttonPrecedent.className = "hidden"
+                buttonTerminer.className = "hidden"
+                buttonSuivant.className = "top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                console.log('premier')
+            }else
+            {
+                currentIndex = (currentIndex - 1);
+                buttonTerminer.className = "hidden"
+                buttonSuivant.className = "top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                element_precedent =currentIndex + 1;
+                afficherElement(currentIndex, hideClass, element_precedent)
+            }
+            });
+
+         } else {
+        console.log('Cliquer sur demarrer pour charger les question')
+        // La variable n'est pas définie
+        }
+    </script>
+    <script>
+        //script pour le chrono duree evaluation
         function paddedFormat(num) {
             return num < 10 ? '0' + num : num;
         }
