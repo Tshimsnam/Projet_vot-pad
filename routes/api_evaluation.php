203f2502308155e9a053b3bad85d4cf25e2d40ad
MrAlainZoura <t.alain1312@gmail.com>
2024-07-11 17:00:34 +0100

    api question_ phases

--- routes/api_evaluation.php
+++ routes/api_evaluation.php
@@ -2,8 +2,11 @@
 
 use App\Http\Controllers\Api\PhaseController;
 use App\Http\Controllers\Api\QuestionController;
+use App\Http\Controllers\Api\QuestionPhaseController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/phases', [PhaseController::class ,'index']);
  Route::apiResource('/phases', PhaseController::class, ['as'=> 'api'])->middleware('auth:sanctum');
  Route::apiResource('/questions', QuestionController::class, ['as'=> 'api'])->middleware('auth:sanctum');
+ Route::apiResource('/question_phase', QuestionController::class, ['as'=> 'api']);
+ Route::get('question_phases/{id}', [QuestionPhaseController::class,'shows'])->name('question_phases');
