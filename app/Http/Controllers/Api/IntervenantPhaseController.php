a0866960b30b3c652c021a03d72193d21ec1faff
hena_ndombele <henandombe2021@gmail.com>
2024-09-16 16:58:28 +0100

    Modification de l'api IntervenantPhase pour qu'il fasse une recherche du vote par candidat

--- app/Http/Controllers/Api/IntervenantPhaseController.php
+++ app/Http/Controllers/Api/IntervenantPhaseController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 use App\Models\IntervenantPhase;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\IntervenantPhaseResource;
+use App\Models\Jury;
+use App\Models\Vote;
 
 class IntervenantPhaseController extends Controller
 {
@@ -29,18 +31,32 @@ class IntervenantPhaseController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show($phaseId)
+    public function show(Request $request, $phaseId)
     {
+        $authorizationHeader = $request->header('Authorization');
+        $authorizationHeader = $request->header('Authorization');
+
+        if (preg_match('/Bearer\s(\S+)/', $authorizationHeader, $matches)) {
+            $token = $matches[1];
+        } 
+
+        $jury = Jury::where('token', $token)->first();
+
         $intervenantPhases = IntervenantPhase::where('phase_id', $phaseId)->get();
         $intervenants = [];
-    
         foreach ($intervenantPhases as $intervenantPhase) {
             $intervenant = Intervenant::find($intervenantPhase->intervenant_id);
             $intervenant->intervenantPhaseId = $intervenantPhase->id;
+            $exist = Vote::where('jury_phase_id', $jury->id)->where('intervenant_phase_id', $intervenantPhase->id)->exists();
+            if($exist){
+                $intervenant->isDone = true;
+            }else{
+                $intervenant->isDone = false;
+            }
             $intervenant->phaseId = (int) $phaseId;
             $intervenants[] = $intervenant;
         }
-    
+
         return IntervenantPhaseResource::collection($intervenants);
     }
 
