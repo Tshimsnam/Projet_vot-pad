e21faf09be1586b9f1fda2eb8a5e12e3b5fda89b
yoshi <glodykabengele55@gmail.com>
2024-10-21 13:18:38 +0100

    correction de refresh de vote automatique

--- resources/views/votes/show.blade.php
+++ resources/views/votes/show.blade.php
@@ -66,10 +66,11 @@
                                                 d="M 43.077 63.077 c -0.046 0 -0.093 -0.001 -0.14 -0.002 c -1.375 -0.039 -2.672 -0.642 -3.588 -1.666 L 23.195 43.332 c -1.84 -2.059 -1.663 -5.22 0.396 -7.06 c 2.059 -1.841 5.22 -1.664 7.06 0.396 l 12.63 14.133 l 38.184 -38.184 c 1.951 -1.952 5.119 -1.952 7.07 0 c 1.953 1.953 1.953 5.119 0 7.071 L 46.612 61.612 C 45.674 62.552 44.401 63.077 43.077 63.077 z"
                                                 style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,165,16); fill-rule: nonzero; opacity: 1;"
                                                 transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
-                                            <path d="M 45 90 C
-                                                        20.187 90 0 69.813 0 45 C 0 20.187 20.187 0 45 0 c 2.762 0 5 2.239 5 5 s -2.238
-                                                        5 -5 5 c -19.299 0 -35 15.701 -35 35 s 15.701 35 35 35 s 35 -15.701 35 -35 c 0
-                                                        -2.761 2.238 -5 5 -5 s 5 2.239 5 5 C 90 69.813 69.813 90 45 90 z"
+                                            <path
+                                                d="M 45 90 C
+                                                                                20.187 90 0 69.813 0 45 C 0 20.187 20.187 0 45 0 c 2.762 0 5 2.239 5 5 s -2.238
+                                                                                5 -5 5 c -19.299 0 -35 15.701 -35 35 s 15.701 35 35 35 s 35 -15.701 35 -35 c 0
+                                                                                -2.761 2.238 -5 5 -5 s 5 2.239 5 5 C 90 69.813 69.813 90 45 90 z"
                                                 style="stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10; fill: rgb(0,165,16); fill-rule: nonzero; opacity: 1;"
                                                 transform=" matrix(1 0 0 1 0 0) " stroke-linecap="round" />
                                         </g>
@@ -133,13 +134,18 @@
     </section>
 
     <script>
-        window.addEventListener("load", () => {
-            if (localStorage.getItem("refreshPreviousPage")) {
-                localStorage.removeItem("refreshPreviousPage");
-                location.reload();
-            }
-        });
-
+        if (localStorage.getItem("refreshPreviousPage") === "true") {
+            console.log('back succes');
+            
+            let candidats = {{ $candidats }};
+            let jury_id = {{ $jury_id }};
+            let phase_id = {{ $phase_id }};
+            let criteresIds = {{ $criteres }};
+            let nombreUser = {{ $nombreUser }};
+            getLocalStorage(phase_id, candidats, jury_id, criteresIds, nombreUser);
+            
+            localStorage.removeItem("refreshPreviousPage");
+        }
         const searchInput = document.getElementById('search');
         const items = document.querySelectorAll('.grid > div');
 
