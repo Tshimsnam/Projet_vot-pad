8f2c19bdd1f99d5258c45022df75011411f64c82
yoshi <glodykabengele55@gmail.com>
2024-12-11 15:29:44 +0100

    configuration .env exemple

--- resources/views/mails/sendMail.blade.php
+++ resources/views/mails/sendMail.blade.php
@@ -109,12 +109,12 @@
         </div>
 
         <div class="bg-white dark:bg-gray-800 shadow p-2 mb-3">
-            <form class="flex justify-between " action="">
-                <div class="mb-3">
+            <form class="flex justify-between gap-36" action="">
+                <div class=" flex-1 mb-3">
                     <label for="objet" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Objet du
                         mail</label>
                     <input type="text" id="objet" name="objet"
-                        class="w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Saisissez l'objet du mail" required />
                 </div>
                 <div class="flex gap-2" id="">
